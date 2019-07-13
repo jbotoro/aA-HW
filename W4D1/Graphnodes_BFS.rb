b2d39b8bd3e20b7f98c2cfb1d12f2c7213583d3d
Jordan Black <jblack530@gmail.com>
2019-07-13 12:55:42 -0700

    added visited as argument for #bfs and set its default value to Set.new , in iteration of neighbors added code to return nil if visited include value of given neighbor and added neighbor value into visited set

--- W4D1/Graphnodes_BFS.rb
+++ W4D1/Graphnodes_BFS.rb
@@ -1,3 +1,5 @@
+require "set"
+
 class GraphNode
     attr_accessor :value, :neighbors
     
@@ -19,13 +21,19 @@ c.neighbors = [b, d]
 e.neighbors = [a]
 f.neighbors = [e]
 
-def bfs(start_node, target_value)
+def bfs(start_node, target_value,visited = Set.new())
+
+
     queue = [start_node]
 
     until queue.empty?
         ele =queue.shift
         return ele if ele.value == target_value
-        ele.neighbors.each { |neighbor| queue << neighbor }
+        ele.neighbors.each do |neighbor|
+            queue << neighbor
+            return nil if visited.include?(neighbor.value)
+            visited << neighbor.value 
+        end
     end
     
     nil
