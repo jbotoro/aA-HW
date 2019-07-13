d71743cb14908b1a998bf4fc0de8372855488a05
Jordan Black <jblack530@gmail.com>
2019-07-13 12:36:14 -0700

    created sample graphnode tree, created #bfs methods outside of graphnode class, tested bfs for target val of f , caught in infinite loop

--- W4D1/Graphnodes_BFS.rb
+++ W4D1/Graphnodes_BFS.rb
@@ -1,7 +1,34 @@
 class GraphNode
     attr_accessor :value, :neighbors
+    
     def initialize(value)
         self.value = value
         self.neighbors = []
     end
+
 end
+
+a = GraphNode.new('a')
+b = GraphNode.new('b')
+c = GraphNode.new('c')
+d = GraphNode.new('d')
+e = GraphNode.new('e')
+f = GraphNode.new('f')
+a.neighbors = [b, c, e]
+c.neighbors = [b, d]
+e.neighbors = [a]
+f.neighbors = [e]
+
+def bfs(start_node, target_value)
+    queue = [start_node]
+
+    until queue.empty?
+        ele =queue.shift
+        return ele if ele.value == target_value
+        ele.neighbors.each { |neighbor| queue << neighbor }
+    end
+    
+    nil
+end
+
+p bfs(a, "f")
