667a9c03bb2164ad19972152ae1455c8d2510c15
Jordan Black <jblack530@gmail.com>
2019-08-08 19:58:22 -0700

    W7D4 hw' git status

--- W7D1/99cats auth solution/app/mailers/user_mailer.rb
+++ W7D1/99cats auth solution/app/mailers/user_mailer.rb
@@ -1,2 +1,10 @@
 class UserMailer < ApplicationMailer
+
+    default from: 'contact@appacademy.io'
+
+    def welcome_email(user)
+        @user = user
+
+        mail(to: @user.username, subject: 'Welcome to My Awesome 99 Cats Site')
+    end
 end
